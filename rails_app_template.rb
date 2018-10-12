12116852777f83617f87e049a359308674f78339
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2018-10-12 12:03:34 -0500

    Update Gemfile with the newest versions of a common Rails app.

--- rails_app_template.rb
+++ rails_app_template.rb
@@ -12,7 +12,7 @@ add_source 'https://rubygems.org'
 
 gem 'rails', '~> 5.2.1'
 gem 'pg', '>= 0.18', '< 2.0'
-gem 'puma', '~> 3.7'
+gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
@@ -30,6 +30,7 @@ gem_group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'chromedriver-helper'
   gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'shoulda-matchers'
